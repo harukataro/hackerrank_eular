93f5afe83979be622b178cae69cafb3d7726ca99
i <isao@miko.local>
2018-12-29 14:01:34 +0900

    LargestPalindromeProduct.py

---
+++ LargestPalindromeProduct.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import sys
+
+def is_palindrome6dig(n):
+    maxpal = 101101
+    for i in range(999,101, -1):
+        for j in range(999,101, -1):
+            x = i * j
+            if x <= maxpal:
+                break
+            else:    
+                xstr = str(x)
+                if xstr[5] == xstr[0] and xstr[4] == xstr[1] and xstr[3] == xstr[2] and x<n:
+                    maxpal = x
+    return maxpal
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(is_palindrome6dig(n))
+
